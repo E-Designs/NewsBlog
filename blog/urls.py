290c512763d5a661e13cd43a1ed2ae90c75a3c12
E-Design <ehlh5773@gmail.com>
2021-11-07 17:30:23 -0800

    Added an aothors post page.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
     path('post/about', views.post_about, name='post_about'),
+    path('post/authors-posts/<int:find_author>', views.post_authors_posts, name='post_authors_posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.db.models import Q 
 
 # Create your views here.
 def post_about(request):
     return render(request, 'blog/post_about.html')
     
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(Q(published_date__lte=timezone.now()), Q(visability='p') | Q(visability='a')).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_authors_posts(request, find_author):
+    posts = Post.objects.filter(author=find_author).order_by('published_date')
+    pic = posts[0].author.profile.image.url
+    auth = posts[0].author
+    context = {
+        'posts': posts,
+        'pic': pic,
+        'auth': auth
+    }
+    return render(request, 'blog/post_authors_posts.html', context)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -22,6 +35,7 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
+            post.visability = 'p'
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
