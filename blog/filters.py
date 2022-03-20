e8c431ec63710d9db7b428bd3283c877b9d6627d
E-Design <ehlh5773@gmail.com>
2022-03-20 15:03:35 -0700

    Made filter form a toggle form.

--- blog/filters.py
+++ blog/filters.py
@@ -4,4 +4,4 @@ from .models import Post
 class postFilter(django_filters.FilterSet):
     class Meta:
         model = Post
-        fields = ['author','title',]
+        fields = ['author','title', 'subject', 'city', 'state',]
