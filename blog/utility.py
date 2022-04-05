d6856475dae82b07823231d8197b7f4c93e5a8c9
E-Design <ehlh5773@gmail.com>
2022-04-04 18:33:57 -0700

    Updated taglines

--- blog/utility.py
+++ blog/utility.py
@@ -7,5 +7,5 @@ class Visability_State:
     approved = 'approved'
     hidden = 'hidden'
 
-taglines = ['This is not your parents news site!', 'The only site that allows users to find the truth!', 'Every news story on this site is true or fake!', 'If you are gullible and can not tell what is true, leave this site now!', 
-'This site is for entertainment only!', 'This site allows you to be a reporter!', 'The truth is out there!', 'The place where people come to read, report, or talk about new.']
+taglines = ["This is not your parents’ news site!", 'The only site that allows users to find the truth!', 'Are these news stories true or fake?', "If you can’t tell what the truth is, leave this site now!", 
+'This site is for entertainment only!', 'This site allows you to be a reporter!', 'The truth is out there!', 'The place where people come to read, report, or talk about news.']
